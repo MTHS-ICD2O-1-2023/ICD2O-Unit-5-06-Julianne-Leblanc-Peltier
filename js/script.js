f66027bd885bdf830966e4eae1cc09b6cb262a60
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-04 03:22:21 +0000

    did js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,17 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  const firstNumber = parseInt(document.getElementById("first-number").value)
+  const secondNumber = parseInt(document.getElementById("second-number").value)
+  let counter = 0
+  let answer = 0
+
+  while (counter < secondNumber) {
+    answer += firstNumber
+    counter++
+  }
+
+  document.getElementById("answer").innerHTML = answer
+} 
