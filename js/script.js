8d272027348ae245bc69ff7fbffab88331ed4d32
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-06 18:37:03 +0000

    fixed comment credit

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Julianne Leblanc-Peltier
+// Created on: May 2024
 // This file contains the JS functions for index.html
 
 function myButtonClicked() {
