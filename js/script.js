473e4a41a47182b012f9a5c44fbcf8d5daac5b99
Julianne <144264326+Julianne-Leblanc-Peltier@users.noreply.github.com>
2024-05-06 18:33:03 +0000

    added flowchart + fixed storyboard

--- js/script.js
+++ js/script.js
@@ -15,10 +15,9 @@ function myButtonClicked() {
     counter++
   }
 
-  if ((firstNumber < 0) && (secondNumber > 0) || (firstNumber > 0) && (secondNumber < 0)) {
+  if (((firstNumber < 0) && (secondNumber > 0)) || ((firstNumber > 0) && (secondNumber < 0))) {
     answer = answer * -1
   } 
 
   document.getElementById("answer").innerHTML = firstNumber + " x " + secondNumber + " = " + answer
-
 } 
